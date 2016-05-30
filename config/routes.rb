1b85b6b9526699882af8b554da921b8877fbed61
AntoinePetit24 <antoinepetit@live.fr>
2016-05-30 11:53:43 +0800

    first version w booststrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'resume_pages#home2'
+  root 'resume_pages#home_bootstrap'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
