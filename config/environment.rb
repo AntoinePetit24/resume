1937b0034b8cefe5cc3081b3d72d6823c856e206
AntoinePetit24 <antoinepetit@live.fr>
2016-06-02 12:37:33 +0800

    switched back environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# I recommend using this line to show error
-config.action_mailer.raise_delivery_errors = true
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :domain         => 'mail.google.com',
-  :port           => 587,
-  :user_name      => 'antoine.petit.callmann@gmail.com',
-  :password       => 'kuja24GMAIL',
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
