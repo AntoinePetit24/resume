ac40d5f6081888b526070ca65d218cbf9d3cd424
AntoinePetit24 <antoinepetit@live.fr>
2016-06-03 17:29:35 +0800

    modified footer + haml format instead of html

--- app/helpers/resume_pages_helper.rb
+++ app/helpers/resume_pages_helper.rb
@@ -1,2 +1,14 @@
 module ResumePagesHelper
+  # Returns the full title on a per-page basis.
+  #def abilities_title(title)
+  #  content_tag :span, title, class: "ability-title"
+  #end
+
+  #def abilities_star(star_number)
+  #  for i in 1..star_number
+  #    #content_tag (:span, :class => "glyphicon glyphicon-star filled")
+  #    content_tag :span, class: ["glyphicon", "glyphicon-star", "filled"]
+  #  end
+
+  #end
 end
